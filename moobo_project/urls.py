eea1b179210d30db1c06940c1ac1be94152fbd7d
Gadji <lioknuckles@gmail.com>
2020-04-08 20:56:22 +0300

    Add files via upload

--- moobo_project/urls.py
+++ moobo_project/urls.py
@@ -25,17 +25,11 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-<<<<<<< HEAD
     path('designer/sign_in/', LoginView.as_view(template_name='designer/sign_in.html'), name='designer-sign-in'),
     path('designer/sign_out/', LogoutView.as_view(next_page='/'), name='designer-sign-out'),
     path('designer/', views.authapp_home, name='authapp-home'),
     path('designer/sign_up', views.authapp_sign_up, name='sign-up'),
     path('designer/newproject', views.new_project, name='new-project'),
-=======
-    path('designer/sign_up/', views.home, name='sign-up'),
-    path('designer/sign_in/', LoginView.as_view(template_name='designer/sign_in.html'), name='designer-sign-in'),
-    path('designer/sign_out/', LogoutView.as_view(next_page='designer/projects.html'), name='designer-sign-out'),
->>>>>>> 530d3d7cf7d42d55a7bc030dfea7731923bde2d6
 
 
     # path('designer/', views.designer_home, name='designer-gome'),
